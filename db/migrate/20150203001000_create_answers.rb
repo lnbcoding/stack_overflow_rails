e9833404241075048f140110cf308d60c2eef04e
lnbcoding <leonardbui@gmail.com>
2015-02-03 15:54:00 -0800

    add in migration for question has many answers

--- db/migrate/20150203001000_create_answers.rb
+++ db/migrate/20150203001000_create_answers.rb
@@ -1,10 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      t.string :title
-      t.text :content
+    t.string :title
+    t.text :content
+    t.references :question, index: true
 
-      t.timestamps null: false
+    t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,9 +19,12 @@ ActiveRecord::Schema.define(version: 20150203001000) do
   create_table "answers", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
