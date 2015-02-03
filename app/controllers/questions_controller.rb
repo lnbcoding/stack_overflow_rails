ce6625f8dfa543bd4eacd1af696acb3ba917b617
lnbcoding <leonardbui@gmail.com>
2015-02-02 17:19:37 -0800

    Finish Release 1 but no associations

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+
+    def index
+        @questions = Question.all
+    end
+
+    def new
+    end
+
+    def show
+        @question = Question.find(params[:id])
+    end
+
+end
